eb5c55c52579024daba43a9070a00685a2e81c80
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-15 12:32:13 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+element.innerHTML = "Hi Hi, hello";
+element.style.backgroundColor = "00000";
+document.body.appendChild(element);
+element.style.textAlign = 'center';
+
